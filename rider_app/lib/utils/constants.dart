f6ce690ad33b64675d2c796e6a089e65715eb692
Carine-69 <umugabecarine@gmail.com>
2026-05-15 13:03:51 +0200

    Restructure: rename rider_app to RwandaRide, remove old folders

--- rider_app/lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  static const String baseUrl = 'http://127.0.0.1:8000';
-  static const String tokenKey = 'auth_token';
-  static const String userRoleKey = 'user_role';
-  static const String userNameKey = 'user_name';
-  static const String userIdKey = 'user_id';
-}
